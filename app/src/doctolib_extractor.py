f0d41c5453a1fba2c90776ec0f31e3f4f456fc80
Mathias Gout <mathias.gout@gmail.com>
2023-08-06 18:11:14 +0200

    new headers

--- app/src/doctolib_extractor.py
+++ app/src/doctolib_extractor.py
@@ -45,25 +45,19 @@ class DoctolibExtrator:
             "Host": "www.doctolib.fr",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            # "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-            # "Accept-Encoding": "gzip, deflate, br",
-            # "DNT": "1",
-            # "Connection": "keep-alive",
-            # "Upgrade-Insecure-Requests": "1",
-            # "Sec-Fetch-Dest": "document",
-            # "Sec-Fetch-Mode": "navigate",
-            # "Sec-Fetch-Site": "none",
-            # "Sec-Fetch-User": "?1",
-            # "Pragma": "no-cache",
-            # "Cache-Control": "no-cache",
+            "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
         }
-
-        if page == 1:
-            url = f"https://www.doctolib.fr/{self.speciality}/{self.place}"
-        else:
-            url = f"https://www.doctolib.fr/{self.speciality}/{self.place}?page={page}"
-
-        r_get = requests.get(url, headers=headers)
+        r_get = requests.get(f"https://www.doctolib.fr/{self.speciality}/{self.place}?page={page}", headers=headers)
         logger.debug(f"{r_get.url} (code={r_get.status_code})")
 
         if r_get.ok:
