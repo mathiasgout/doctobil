1c5f8a65d042c4685279b6f16a4b2c9a21426252
Mathias Gout <mathias.gout@gmail.com>
2023-09-21 23:12:31 +0200

    added arg

--- doctobil/browser.py
+++ doctobil/browser.py
@@ -25,9 +25,10 @@ class DoctolibBrowser:
 
     def _init_driver(self) -> webdriver.Chrome:
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument('--no-sandbox')
         options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         # To get acces to browser logs (https://stackoverflow.com/questions/52633697/selenium-python-how-to-capture-network-traffics-response)
         options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
